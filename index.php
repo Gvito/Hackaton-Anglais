bdc21e9c1af6234c49f616c1be8c383636b1513f
Sebastien <sebastien.munoz@outlook.fr>
2018-10-10 10:23:21 +0200

    Add form connect user with css

--- index.php
+++ index.php
@@ -22,7 +22,35 @@
   <![endif]-->
 
   <!-- Add your site or application content here -->
-  <p>Hello world! This is HTML5 Boilerplate.</p>
+  <main>
+    <!-- center form and logo -->
+    <div class="centre">
+      <!-- logo -->
+      <div class="logo">
+        <img src="https://www.adep-roubaix.fr/img/adep-logo-cut.png" alt="Logo du site">
+      </div>
+      <!-- end logo -->
+      
+      <!-- title page -->
+      <h1>Espace connexion utilisateur</h1>
+      <!-- end title page -->
+
+      <!-- form -->
+      <div class="connexion">
+        <form method="post" action="/index.php">
+          <label>Votre nom:</label>
+          <input type="text" name="nom">
+          <label>Votre prénom:</label> 
+          <input type="text" name="prenom">
+          <label>Votre clé:</label>
+          <input type="text" name="cle">
+          <input class="valider" type="submit" value="Se connecter">
+        </form> 
+        <!-- end form -->
+      </div>
+    </div>
+    <!-- end center form and logo -->
+  </main>
   <script src="js/vendor/modernizr-3.6.0.min.js"></script>
   <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
   <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
