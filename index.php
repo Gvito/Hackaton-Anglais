b4eed8cd2dce843b3e0055c971af8d2d435f4446
Sebastien <sebastien.munoz@outlook.fr>
2018-10-10 15:09:18 +0200

    update html/css and add font

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title></title>
+  <title>Index</title>
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -14,6 +14,9 @@
 
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/main.css">
+
+  <!-- font-->
+  <link rel="stylesheet" href="../font/css/font-awesome.min.css">
 </head>
 
 <body>
@@ -21,7 +24,6 @@
     <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
   <![endif]-->
 
-  <!-- Add your site or application content here -->
   <main>
     <!-- center form and logo -->
     <div class="centre">
---
+++ templates/header.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Header</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" type="text/css" href="../css/normalize.css">
+  <link rel="stylesheet" type="text/css" href="../css/main.css">
+
+  <!-- font-->
+  <link rel="stylesheet" href="../font/css/font-awesome.min.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <aside>
+                <img src="https://www.adep-roubaix.fr/img/adep-logo-cut.png" alt="Logo du site">
+                <p>Adeline</p>
+            </aside>
+            <aside class="icones">
+                <i class="fa fa-user fa-2x" aria-hidden="true"></i>
+                <i class="fa fa-book fa-2x" aria-hidden="true"></i>
+                <i class="fa fa-times fa-2x" aria-hidden="true"></i>
+            </aside>
+        </nav>
+    </header>
